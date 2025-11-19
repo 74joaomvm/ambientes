458f941f7146a16e9f31d1b250f957d31c2f449e
Joao Mendonça <joaomvm.2006@gmail.com>
2025-11-19 19:44:09 +0000

    Adicionado o ficheiro de conexão do ficheiro python com a base de dados, editar para funcionar no seu codigo

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import mysql.connector
 from datetime import datetime
+
+
+
+# ------------------------------------
+# CONEXÃO COM MYSQL
+# ------------------------------------
+def conectar():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",        # coloca o teu utilizador
+        password="",        # coloca a tua password
+        database="git"      # base de dados que aparece no HeidiSQL
+    )
