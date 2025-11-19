9c195aa1e7f48ae82d4b91eed3d9fd440d1092ce
José Martins <a89810@ualg.pt>
2025-11-19 17:17:38 +0000

    Adicionada a tabela Produto

--- database/database.sql
+++ database/database.sql
@@ -5,3 +5,14 @@ CREATE TABLE fornecedor (
     email TEXT,
     morada TEXT
 );
+
+CREATE TABLE produto (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    categoria TEXT,
+    preco REAL NOT NULL,
+    stock INTEGER DEFAULT 0,
+    fornecedor_id INTEGER,
+    ativo BOOLEAN DEFAULT 1,
+    FOREIGN KEY (fornecedor_id) REFERENCES fornecedor(id)
+);
